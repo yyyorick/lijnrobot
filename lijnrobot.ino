224e4c7dbe6634f6bf7f8fa118025f0ceaaaa20f
Yorick te Riele <114359401+yyyorick@users.noreply.github.com>
2024-03-06 18:53:37 +0100

    klaar

---
+++ lijnrobot.ino
@@ -0,0 +1,25 @@
+bool sensorValues[] = {false, false, false, false, false};
+int start = 0;
+
+void setup() {
+    Serial.begin(9600);
+
+}
+void loop() {
+    readSensor(sensorValues);
+    for (int i = 0; i < 5; i++) {
+        Serial.print(sensorValues[i]);
+    }
+    Serial.println();
+    delay(1000);
+}
+void readSensor(bool sensorValues[]){
+    for(int i = 0; i < 5; i++){
+        if (analogRead(i) > 500){
+            sensorValues[i] = true;
+        } else {
+            sensorValues[i] = false;
+        }
+    }
+    return sensorValues;
+}
