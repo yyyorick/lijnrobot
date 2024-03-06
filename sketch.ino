859dfac8ad816eeecff20259049c26665aa74ae2
Yorick te Riele <114359401+yyyorick@users.noreply.github.com>
2024-03-06 16:19:03 +0100

    gfihjoph

--- sketch.ino
+++ sketch.ino
@@ -1,24 +1,17 @@
+
 int start = 0;
 
 void setup() {
     Serial.begin(9600);
+    
 }
 void loop() {
-    analogRead(A0);
-    Serial.print("A0: ");
-    Serial.println(analogRead(A0));
-    analogRead(A1);
-    Serial.print("A1: ");
-    Serial.println(analogRead(A1));
-    analogRead(A2);
-    Serial.print("A2: ");
-    Serial.println(analogRead(A2));
-    analogRead(A3);
-    Serial.print("A3: ");
-    Serial.println(analogRead(A3));
-    analogRead(A4);
-    Serial.print("A4: ");
-    Serial.println(analogRead(A4));
-    delay(1000);
-
+    
+}
+bool readSensor(){
+    bool sensorValues[5] = {false, false, false, false, false};
+    for(int i = 0; i < 5; i++){
+        sensorValues[i] = digitalRead(i);
+    }
+    return sensorValues;
 }
