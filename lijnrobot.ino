26c6c15c7672338f7c2a2877d8cc046767c3785d
Peter Bos <s1200965@st.windesheim.nl>
2024-03-07 14:24:39 +0100

    Navigation code

--- lijnrobot.ino
+++ lijnrobot.ino
@@ -3,6 +3,12 @@
 bool sensorValues[] = {false, false, false, false, false};
 int sensorPins[] = {5, A2, A3, A4, A5};
 
+bool straightPath[] = {false, false, true, false, false};
+bool leftTurn[] = {true, true, true, false, false};
+bool rightTurn[] = {false, false, true, true, true};
+bool crossing[] = {true, true, true, true, true};
+//bool offRoad[] = {false, false, false, false, false}; might be redundant?
+
 //Motors
 int pwmSpeed = 75;
 //A
@@ -16,6 +22,11 @@ int brakePinB = 8;
 bool directionState;
 
 #define TCCR2B & B11111000 | B0000111; // for PWM frequency of 30.64 Hz
+
+//Navigation algorithm
+enum robotStatus {idle, forward, leftDetected, rightDetected, crossingDetected, stopped, obstacleDetected};
+robotStatus currentStatus = idle;
+bool followLeft = true;
  
 //---Runtime logic---
 void setup() {
@@ -34,13 +45,12 @@ void setup() {
     
 }
 void loop() {
-    readSensor();
-    for (int i = 0; i < 5; i++) {
-        Serial.print(sensorValues[i]);
-    }
-    Serial.println();
-    turn(true);
+   debugSensorOutput();
+   translateSensor();
+   Navigate();
+
     delay(1000);
+
 }
 
 //---Function definitions---
@@ -51,7 +61,53 @@ void readSensor(){
     }
 }
 
+void translateSensor()
+{
+    if(isBoolArrayEqual(sensorValues, straightPath, 5, 5))
+    {
+        SetRobotState(forward);
+    }
+    else if(isBoolArrayEqual(sensorValues, leftTurn, 5, 5))
+    {
+        SetRobotState(leftDetected);
+    }
+    else if(isBoolArrayEqual(sensorValues, rightTurn, 5, 5))
+    {
+        SetRobotState(rightDetected);
+    }
+    else if(isBoolArrayEqual(sensorValues, crossing, 5, 5))
+    {
+        SetRobotState(crossingDetected);
+    }
+    // else
+    // {
+        //fullStop();
+        //SetRobotState(stopped);
+    //}
+
+}
+
+void debugSensorOutput()
+{
+    readSensor();
+    for (int i = 0; i < 5; i++) {
+        Serial.print(sensorValues[i]);
+    }
+    Serial.println();
+    // turn(true);
+    // delay(1000); temporary code, disabled to test robot states
+}
+
 //Movement
+void fullStop()
+{
+    digitalWrite(pwmSpeedPinA, LOW);
+    digitalWrite(pwmSpeedPinB, LOW);
+
+    digitalWrite(brakePinA, HIGH);
+    digitalWrite(brakePinB, HIGH);
+
+}
 void driveForward(){
     //Vroem
     digitalWrite(directionPinA, HIGH);
@@ -80,4 +136,68 @@ void turn(bool left) {
         digitalWrite(brakePinB, LOW);       
         analogWrite(pwmSpeedPinB, 75);     
     }
+}
+
+//Navigation
+void Navigate()
+{
+    if(currentStatus != obstacleDetected)
+    {
+        switch (currentStatus)
+        {
+        case idle:
+            Serial.println("I am idle!");
+            break;
+        case forward:
+            Serial.println("I am going forward!");
+            break;
+        case leftDetected:
+            Serial.println("Left turn detected!");
+            break;
+        case rightDetected:
+            Serial.println("Right turn detected!");
+            break;
+        case crossingDetected:
+            Serial.println("Crossing (Or endpoint) detected!");
+            break;
+        case stopped:
+            Serial.println("I have stopped!");
+            //Probably need to turn around?
+            break;
+        default:
+            Serial.println("De robot is stukkie wukkie :3");
+            //fullStop();
+            break;
+        }
+    }
+    else
+    {
+        //Navigate around the obstacle
+    }
+        
+}
+
+void SetRobotState(robotStatus newState)
+{
+    currentStatus = newState;
+}
+
+//---Helper functions---
+bool isBoolArrayEqual(bool* a1, bool* a2, int a1Count, int a2Count)
+{
+   if(a1Count != a2Count)
+   {
+    return false; //Arrays cannot be equal if their counts are different.
+   }
+
+   for (int i = 0; i < a2Count; i++)
+   {
+        if(a1[i] != a2[i])
+        {
+            return false; //Return false if a difference is detected.
+        }
+   }
+   
+   return true; //Arrays are the same.
+    
 }
